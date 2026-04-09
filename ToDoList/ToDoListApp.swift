ec0fc53596f1548b3ccc6ae55ed7f09be9ea338a
Elia Etzanidi <eliaetz@gmail.com>
2026-04-09 13:12:23 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Elia Etzanidi on 9/4/26.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
