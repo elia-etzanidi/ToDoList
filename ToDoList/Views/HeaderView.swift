d31bd4cacee70f0e46ee587ca3aebc1be3d73f6b
Elia Etzanidi <eliaetz@gmail.com>
2026-04-16 17:35:04 +0300

    done header for login view

--- ToDoList/Views/HeaderView.swift
+++ ToDoList/Views/HeaderView.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct HeaderView: View {
+    
+    // screen width helper
+    private var screenWidth: CGFloat {
+        let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        return scene?.screen.bounds.width ?? 375
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            RoundedRectangle(cornerRadius: 0)
+                .foregroundColor(.pink)
+                .rotationEffect(Angle(degrees: 15))
+            
+            
+            VStack {
+                Text("To Do List")
+                    .font(.system(size: 50, weight: .bold))
+                    .foregroundColor(Color(.white))
+                Text("Get Things Done")
+                    .font(.system(size:30))
+                    .foregroundStyle(Color(.white))
+            }
+            .padding(.top, 40)
+        }
+        .frame(width: screenWidth * 3,
+               height: 300)
+        .offset(y: -100)
     }
 }
 
