67422f5c286b73da77d6ba0ce704f37e7d93e8b6
Elia Etzanidi <eliaetz@gmail.com>
2026-04-16 17:34:19 +0300

    done header for login view

---
+++ ToDoList/Views/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  ToDoList
+//
+//  Created by Elia Etzanidi on 16/4/26.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
