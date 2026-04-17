58fea82876f26a887164eff4be18e5a74af2dd4e
Elia Etzanidi <eliaetz@gmail.com>
2026-04-17 21:56:52 +0300

    finished login view ui

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -9,18 +9,49 @@ import SwiftUI
 
 struct LoginView: View {
     
+    @State var email = ""
+    @State var password: String = ""
+    
     var body: some View {
-        VStack{
-            // Header
-            HeaderView()
-            
-            // Login Form
-            
-            // Create Account
-            
-            Spacer()
+        NavigationView {
+            VStack{
+                // Header
+                HeaderView()
+                
+                // Login Form
+                Form {
+                    TextField("Email Adress", text: $email)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    Button {
+                        // Attempt Login
+                        
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(Color.blue)
+                            
+                            Text("Login")
+                                .foregroundColor(Color.white)
+                                .bold()
+                        }
+                        .frame(height: 35)
+                    }
+                }
+                
+                // Create Account
+                VStack{
+                    Text("New around here?")
+                    NavigationLink("Create an account",
+                                   destination: RegisterView())
+                }
+                .padding(.bottom, 50)
+                
+                Spacer()
+            }
         }
-    }
+        }
 }
 
 #Preview {
