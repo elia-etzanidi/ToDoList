13074c1a62908a72fe167b59dd452d50db5d0811
Elia Etzanidi <eliaetz@gmail.com>
2026-04-23 16:58:19 +0300

    toggle checkmark functionality

--- ToDoList/ViewModels/ToDoListItemViewViewModel.swift
+++ ToDoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import Combine
+import FirebaseAuth
+import FirebaseFirestore
 
 /// ViewModel for single todo item
 
@@ -14,6 +16,18 @@ class ToDoListItemViewViewModel: ObservableObject {
     init() {}
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item //creating a copy because item is a constant
+        itemCopy.setDone(!item.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
     }
 }
